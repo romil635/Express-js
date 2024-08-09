5a352407942ca16696418d9aa3104de86984aa1c
romil635 <139975938+romil635@users.noreply.github.com>
2024-08-09 18:14:47 +0530

    Your commit message here

--- express.js
+++ express.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const server = express();   // create server
-// const data = require('./friend.json');
-// const fs = require('fs');
-// const data = fs.readFileSync('./friend.json', 'utf8');
-// console.log(data);
+const morgan = require('morgan');
+
+server.use(morgan('dev'));
+
+const loggerFun = (req, res, next) => {
+    console.log(req.ip, req.url, req.method);
+    next();
+}
+server.use(loggerFun);
+
+// in-built middleware
+server.use(express.json());
+server.use(express.urlencoded({ extended: false}));
+server.use("/hello", express.static('public'));
 
 const myFun = (req, res, next) =>{
-    // console.log(req.query);
-    if(req.query.age >= 18){
-        console.log('Success');
-        next();
-    }else{
-        res.Json({message: "Sorry you are not allowed to visit this website ..."})
-    }
+    console.log(req.body);
+    next();
+    // if(req.query.age >= 18){
+    //     console.log('Success');
+    //     next();
+    // }else{
+    //     res.Json({message: "Sorry you are not allowed to visit this website ..."})
+    // }
 }
 
 // server.use(myFun);  // application
@@ -33,22 +44,7 @@ server.post('/', (req, res) => {
     res.send('Welcome to Post Method');
 })
 
-// server.put('/', (req, res) => {
-//     res.json({msg:'Hello Put Method Called'});
-// })
-
-// server.patch('/', (req, res) => {
-//     res.status(400);
-//     res.json({msg:'Hello Patch Method Called'});
-// })
-
-// server.get('/user', (req, res) => {
-//     res.json(JSON.parse(data));
-// })
-
-
-
 
 server.listen(8000, () => {
     console.log('Server Start at http://localhost8000');
-}); 
+});
